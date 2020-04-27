85be09d472df11d201a65ff419979d9279c2d905
mmdacheva <59440238+mmdacheva@users.noreply.github.com>
2020-04-28 00:11:32 +0300

    Update Plane.cpp

--- PlaneDataBase/Plane.cpp
+++ PlaneDataBase/Plane.cpp
@@ -11,40 +11,44 @@ unsigned long long Plane::getID() const {
 	return this->id;
 }
 
-void Plane::setID(unsigned long long id) {
+void Plane::setID(long long id) {
 	if (id < 0) {
-		id *= -1;
-		std::cout << "The ID you entered was invalid(a negative number. The record is saved with id*(-1)" << std::endl;
+		this->id = id*(-1);
+		std::cout << "The ID you entered was invalid(a negative number). The record is saved with id*(-1)" << std::endl;
 	}
 
-	this->id = id;
+	else
+		this->id = id;
 }
 
 void Plane::setType(const std::string& type) {
 	if (type == "") {
 		std::cout << "The type you entered was invalid so the record is saved with a default value TYPE" << std::endl;
-		setType("TYPE");
+		this->type = "TYPE";
 	}
-
-	this->type = type;
+	
+	else
+		this->type = type;
 }
 
 void Plane::setPlane(const std::string& plane) {
 	if (plane == "") {
 		std::cout << "The name you entered was invalid so the record is saved with a default value PLANE" << std::endl;
-		setPlane("PLANE");
+		this->plane = "PLANE";
 	}
 
-	this->plane = plane;
+	else
+		this->plane = plane;
 }
 
-void Plane::setFlights(unsigned long long flights) {
+void Plane::setFlights(long long flights) {
 	if (flights < 0) {
-		flights *= -1;
-		std::cout << "The number of flights you entered was invalid(a negative number. The record is saved with flights*(-1)" << std::endl;
+		this->flights = flights*(-1);
+		std::cout << "The number of flights you entered was invalid(a negative number). The record is saved with flights*(-1)" << std::endl;
 	}
 
-	this->flights = flights;
+	else
+		this->flights = flights;
 }
 
 std::ostream& operator<<(std::ostream& stream, const Plane& pl) {
